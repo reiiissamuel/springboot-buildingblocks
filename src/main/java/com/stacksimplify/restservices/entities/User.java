1953f9ad29369a954c613661e56abe9b2aec5faf
Samuel Reis <reiiissamuel@gmail.com>
2019-09-30 11:02:41 -0300

    Jpa base restservices development completed

---
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -0,0 +1,120 @@
+package com.stacksimplify.restservices.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//Entity
+@Entity
+@Table(name = "user")
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(name = "USER_NAME", length=50, nullable=false, unique=true)
+	private String username;
+	
+	@Column(name = "FIRST_NAME", length=50, nullable=false)
+	private String firstname;
+	
+	@Column(name = "LAST_NAME", length=50, nullable=false)
+	private String lastName;
+
+	@Column(name = "EMAIL", length=50, nullable=false)
+	private String email;
+	
+	@Column(name = "ROLE", length=50, nullable=false)
+	private String role;
+	
+	@Column(name = "SSN", length=50, nullable=false, unique=true)
+	private String ssn;
+	
+	//No Argument Constructor 
+	public User() {
+	}
+	
+	//Fields Constructor
+	public User(Long id, String username, String firstname, String lastName, String email, String role, String ssn) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.firstname = firstname;
+		this.lastName = lastName;
+		this.email = email;
+		this.role = role;
+		this.ssn = ssn;
+	}
+	
+	//Getters and Setters
+		public Long getId() {
+			return id;
+		}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", firstname=" + firstname + ", lastName=" + lastName
+				+ ", email=" + email + ", role=" + role + ", ssn=" + ssn + "]";
+	}
+
+	
+	
+	
+	
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101, 'dada@dada', 'Darla', 'Farias', 'admin', 'ssn101', 'dada');
+insert into user values(102, 'fefe@fefe', 'Carlos', 'Regis', 'admin', 'ssn102', 'fefe');
+insert into user values(103, 'sasa@sasa', 'Sam', 'Reis', 'admin', 'ssn103', 'sam');
