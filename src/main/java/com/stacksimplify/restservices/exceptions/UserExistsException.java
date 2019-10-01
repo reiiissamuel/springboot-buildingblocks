1fd1ba6c21158122fc85cf6a0546f6fb8a7fae48
Samuel Reis <reiiissamuel@gmail.com>
2019-10-01 12:06:41 -0300

    ResponseStatusException implementation

---
+++ src/main/java/com/stacksimplify/restservices/exceptions/UserExistsException.java
@@ -0,0 +1,9 @@
+package com.stacksimplify.restservices.exceptions;
+
+public class UserExistsException extends Exception{
+
+	public UserExistsException(String message) {
+		super(message);
+	}
+	
+}
