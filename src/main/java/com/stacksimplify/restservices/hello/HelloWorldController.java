68dd6a6bf5da5674ad3ab9c73132f137b3ff5a64
Samuel Reis <reiiissamuel@gmail.com>
2019-10-17 11:33:41 -0300

    i18n - first commit

--- src/main/java/com/stacksimplify/restservices/hello/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/hello/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.stacksimplify.restservices.hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
 public class HelloWorldController {
-
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
+	
 	//Simple method
 	//URI - /helloworld
 	//GET 
@@ -19,5 +28,15 @@ public class HelloWorldController {
 	@GetMapping("/helloworld-bean")
 	public UserDetails helloWorldBean() {
 		return new UserDetails("Samuel", "Reis" ,"RJ");
+	}
+	
+	@GetMapping("hello-int")
+	public String getMessagesInI18NFormat(@RequestHeader(name = "Accept-Language", required = true) String locale){
+		return messageSource.getMessage("label.hello", null, new Locale (locale));
+	}
+	
+	@GetMapping("hello-int2")
+	public String getMessagesInI18NFormat2(){
+		return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
 	}
 }
