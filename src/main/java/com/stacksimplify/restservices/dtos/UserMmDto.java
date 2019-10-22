55ef0f0b59a0f21d50aa779195bd67dde50d198c
Samuel Reis <reiiissamuel@gmail.com>
2019-10-22 12:44:04 -0300

    ModelMapper - First Commit

---
+++ src/main/java/com/stacksimplify/restservices/dtos/UserMmDto.java
@@ -0,0 +1,39 @@
+package com.stacksimplify.restservices.dtos;
+
+import java.util.List;
+
+import com.stacksimplify.restservices.entities.Order;
+
+public class UserMmDto {
+	
+	private Long userId;
+	private String username;
+	private String firstname;
+	private List<Order> orders;
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+}
